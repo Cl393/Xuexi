925edf7b2ddb33e689a7fa75640b5391424838a2
chenglong <787539058@qq.com>
2020-04-08 18:08:48 +0800

    练习格式化字符串

---
+++ BookStringFormats/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace BookStringFormats
+{
+    public class Book : IFormattable
+    {
+        public string BookName { get; }
+        public string Press { get; }//出版社
+        public double Prean { get; }//价格
+        public string Author { get; }//作者
+        public Book(string bookName, string press, double prean, string author)
+        {
+            BookName = bookName;
+            Press = press;
+            Prean = prean;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return BookName + " " + Prean + " " + Press + " " + Author;
+        }
+        public virtual string ToString(string formats)
+        {
+            return ToString(formats, null);
+        }
+        public string ToString(string formats, IFormatProvider formatProvider)
+        {
+            switch (formats)
+            {
+                case null:
+                case "A":
+                    return ToString();
+                case "N":
+                    return BookName;
+                case "P":
+                    return $"{ BookName}{Prean}";
+                default:
+                    throw new FormatException($"invalid format string {formats}");
+
+            }
+        }
+    }
+}
+
---
+++ BookStringFormats/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using static System.FormattableString;
+
+namespace BookStringFormats
+{
+  public  class Program
+    {
+        static void Main()
+        {
+            var p1 = new Book("Dio的奇妙冒险", "wryyyyy", 777, "荒木老贼");
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p1.ToString("A", null));
+            Console.WriteLine($"{p1:N}");
+            Console.WriteLine($"{p1:P}");
+            Console.ReadLine();
+        }
+
+           
+        }
+    }
