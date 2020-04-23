53d97be990f1d4a395cbbf13ea00c97286086d87
chenglong <787539058@qq.com>
2020-04-23 13:37:12 +0800

    反射练习查询

---
+++ 16_01ReflectionSamples/AttributeSample/Characteristic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttributeSample
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+  public  class Characteristic:Attribute
+    {
+        public string Title { get; set; }
+        public int Number { get; set; }
+        // 有两个必选参数
+        public Characteristic(string title, int number)
+        {
+            Title = title;
+            Number = number;
+        }
+        public Characteristic(string title)
+        {
+            Title = title;
+
+
+    }
+}
--- 16_01ReflectionSamples/AttributeSample/Program.cs
+++ 16_01ReflectionSamples/AttributeSample/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection;
+using System.Text;
+using static AttributeSample.Characteristic;
 
 namespace AttributeSample
 {
@@ -6,7 +9,19 @@ namespace AttributeSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Type ty=typeof(AttributeUsageClass);
+            var Fan = ty.GetMethod("Print");
+            Characteristic text = (Characteristic)Fan.GetCustomAttribute(typeof(Characteristic), true);
+
+            ConstructorInfo[] ciArray = ty.GetConstructors(); //获取类的所有构造函数
+            foreach (var item in text.Title)//所有属性
+            {
+                Console.WriteLine(text.Title);
+                Console.WriteLine("OK");
+            }
+            Console.ReadLine();
+            //Console.WriteLine("Hello World!");
         }
     }
 }
