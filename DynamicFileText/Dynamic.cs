3a3daf2bbfefce7e7f06c82be6da524ecbbd9d70
chenglong <787539058@qq.com>
2020-04-27 15:47:22 +0800

    动态编程

---
+++ DynamicFileText/Dynamic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DynamicFileText
+{
+   public class Dynamic
+    {
+        public IEnumerable<dynamic> ParseFile(string fileName)
+        {
+            var xmList = new List<dynamic>();
+            var fileStream = OpenFile(fileName);
+            if (fileStream != null)
+            {
+                string[] txtLine = fileStream.ReadLine().Split(';').Select(s => s.Trim()).ToArray();
+                //读取文件直到末尾
+                while (fileStream.Peek() > 0)
+                {
+                    string[] dataLine = fileStream.ReadLine().Split(';');
+                    dynamic dyEntity = new ExpandoObject();
+                    for (int i = 0; i < txtLine.Length; i++)
+                    {
+                        //字典集合
+                        ((IDictionary<string, object>)dyEntity).Add(txtLine[i], dataLine[i]);
+                    }
+                    xmList.Add(dyEntity);
+                }
+            }
+            return xmList;
+        }
+        //获取文件
+        private StreamReader OpenFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return new StreamReader(File.OpenRead(fileName));
+            }
+            return null;
+        }
+    }
+}
---
+++ DynamicFileText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DynamicFileText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var helper = new Dynamic();
+            var employeeList = helper.ParseFile("TextFile.txt");
+            foreach (var employee in employeeList)
+            {
+                Console.WriteLine($"{employee.Name} {employee.Sex}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
